070869beca978f2898fe59e11e33396f24faa498
Brandon <brandon.russell.961@gmail.com>
2018-05-13 05:11:44 +0000

    Tiempos ej 3

--- 1/runNcompilescriptshort.sh
+++ 1/runNcompilescriptshort.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-cd 2
+cd 3
 cd openmp
 make
 make sequential
 
-for size in 512 1024 2048
+for size in "200000000" "400000000" "800000000"
 do
     for threads in 2 4
     do
         for runs in 1 2 3 4
         do
             echo -e "./2_openmp $size $threads"
-            ./2_openmp $size $threads
+            ./3_openmp $size $threads
         done
     done
 done
 
-for size in 512 1024 2048
+for size in "200000000" "400000000" "800000000"
 do
     for runs in 1 2 3 4
     do
         echo -e "./2_openmp_seq $size 1"
-        ./2_openmp_seq $size 1
+        ./3_openmp_seq $size 1
     done
 done
